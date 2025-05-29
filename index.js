9464de8b41aa05a1077bec9d858136198f7a0553
Margherita <145118725+Margherita97@users.noreply.github.com>
2025-05-29 15:31:15 +0200

    Create function random1()

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const randomNumber1 = Math.round(Math.random() * 6);
+function random1() {
+
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 if (randomNumber1 === 1) {
     document.querySelector(".img1").src = "./images/dice1.png";
-} if (randomNumber1 === 2) {
+} else if (randomNumber1 === 2) {
     document.querySelector(".img1").src = "./images/dice2.png";
-} if (randomNumber1 === 3) {
+} else if (randomNumber1 === 3) {
     document.querySelector(".img1").src = "./images/dice3.png";
-} if (randomNumber1 === 4) {
+} else if (randomNumber1 === 4) {
     document.querySelector(".img1").src = "./images/dice4.png";
-} if (randomNumber1 === 5) {
+} else if (randomNumber1 === 5) {
     document.querySelector(".img1").src = "./images/dice5.png";
 } else {
     document.querySelector(".img1").src = "./images/dice6.png";
 }
+
+}
+
+random1();
+

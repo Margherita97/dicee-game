d7dfc97b513acf7192ff6f29a228d1c178063b4d
Margherita <145118725+Margherita97@users.noreply.github.com>
2025-05-29 15:33:29 +0200

    Create dice2()

--- index.js
+++ index.js
@@ -20,3 +20,25 @@ if (randomNumber1 === 1) {
 
 dice1();
 
+function dice2() {
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+if (randomNumber2 === 1) {
+    document.querySelector(".img2").src = "./images/dice1.png";
+} else if (randomNumber2 === 2) {
+    document.querySelector(".img2").src = "./images/dice2.png";
+} else if (randomNumber2 === 3) {
+    document.querySelector(".img2").src = "./images/dice3.png";
+} else if (randomNumber2 === 4) {
+    document.querySelector(".img2").src = "./images/dice4.png";
+} else if (randomNumber2 === 5) {
+    document.querySelector(".img2").src = "./images/dice5.png";
+} else {
+    document.querySelector(".img2").src = "./images/dice6.png";
+}
+
+}
+
+dice2();
+

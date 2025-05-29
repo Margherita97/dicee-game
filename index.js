b98ccb351f38608309753c42b5c09867719e4580
Margherita <145118725+Margherita97@users.noreply.github.com>
2025-05-29 15:12:39 +0200

    Complete if statement

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const randomNumber1 = Math.round(Math.random() * 6);
 
 if (randomNumber1 === 1) {
-    document.querySelector(".img1.img2").src = "./images/dice1.png";
+    document.querySelector(".img1").src = "./images/dice1.png";
+} if (randomNumber1 === 2) {
+    document.querySelector(".img1").src = "./images/dice2.png";
+} if (randomNumber1 === 3) {
+    document.querySelector(".img1").src = "./images/dice3.png";
+} if (randomNumber1 === 4) {
+    document.querySelector(".img1").src = "./images/dice4.png";
+} if (randomNumber1 === 5) {
+    document.querySelector(".img1").src = "./images/dice5.png";
+} else {
+    document.querySelector(".img1").src = "./images/dice6.png";
 }
